18ae14b9fd67e5ff3a42928b5fce64cc3e520df5
Shihao Zhou <152165443+giter159@users.noreply.github.com>
2026-03-03 13:26:43 +0800

    Add files via upload

---
+++ Multimodal_IntentDiscovery/CABT/backbones/MethodNets/__init__.py
@@ -0,0 +1,15 @@
+from .USNID import USNIDModel
+from .MCN import MCNModel
+from .CC import CCModel
+from .SCCL import SCCLModel
+from .UMC import UMCModel
+from .CABT import ABMModel
+
+methods_map = {
+    'usnid': USNIDModel,
+    'mcn': MCNModel,
+    'cc': CCModel,
+    'sccl': SCCLModel,
+    'umc': UMCModel,
+    'abm':ABMModel,
+}
